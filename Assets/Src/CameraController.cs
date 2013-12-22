3456d914bdeea2ed58b7e73de358aeefa8eb57f8
mm <mm@redmadrobot.com>
2013-12-22 04:46:43 +0400

    Ground mesh generates correctly. Added camera movement

---
+++ Assets/Src/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	float scrollArea=60;
+	float scrollSpeed=5;
+	float dragSpeed=5;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Transform myTransform = transform;
+
+		var mPosX = Input.mousePosition.x;
+		var mPosY = Input.mousePosition.y;
+		
+		// Do camera movement by mouse position
+		if (mPosX < scrollArea) {myTransform.Translate(Vector3.right * -scrollSpeed * Time.deltaTime);}
+		if (mPosX >= Screen.width-scrollArea) {myTransform.Translate(Vector3.right * scrollSpeed * Time.deltaTime);}
+		if (mPosY < scrollArea) {myTransform.Translate(Vector3.forward * -scrollSpeed * Time.deltaTime);}
+		if (mPosY >= Screen.height-scrollArea) {myTransform.Translate(Vector3.forward * scrollSpeed * Time.deltaTime);}
+		
+		// Do camera movement by keyboard
+		myTransform.Translate(new Vector3(Input.GetAxis("EditorHorizontal") * scrollSpeed * Time.deltaTime,
+		                              Input.GetAxis("EditorVertical") * scrollSpeed * Time.deltaTime, 0) );
+		
+		// Do camera movement by holding down option                 or middle mouse button and then moving mouse
+		if ( (Input.GetKey("left alt") || Input.GetKey("right alt")) || Input.GetMouseButton(2) ) {
+			myTransform.Translate(-new Vector3(Input.GetAxis("Mouse X")*dragSpeed, Input.GetAxis("Mouse Y")*dragSpeed, 0) );
+		}
+	}
+}
--- Assets/Src/TerrainController.cs
+++ Assets/Src/TerrainController.cs
@@ -4,9 +4,14 @@ using System.Collections.Generic;
 
 public class TerrainController : MonoBehaviour {
 
-	Cell[,] map = new Cell[8,8];
+	Cell[,] map = new Cell[16,16];
 	bool meshInitializedInEditor = false;
 	private const float hw = 1;
+
+	List<Vector3> vertices = new List<Vector3>();
+	List<int> triangles = new List<int>();
+	List<Vector2> uvs = new List<Vector2>();
+
 	// Use this for initialization
 	void Start () {
 		GenerateMap();
@@ -49,6 +54,8 @@ public class TerrainController : MonoBehaviour {
 		int h = map.GetUpperBound(0);
 		int w = map.GetUpperBound(1);
 
+
+
 		
 		for(int i=0;i<=h;i++)
 		{
@@ -63,15 +70,60 @@ public class TerrainController : MonoBehaviour {
 		//map[1,1].Digged=true;
 	}
 
+
+	/*void AttachHRect(Vector2 p1, Vector3 p2, float z)
+	{
+		int idx = vertices.Count;
+		vertices.Add(new Vector3(p1.x,z,p1.y));
+		uvs.Add(new Vector2(0,0));
+		vertices.Add(new Vector3((i+1)*hw,level,j*hw));
+		uvs.Add(new Vector2(1,0));
+		vertices.Add(new Vector3(i*hw,level,(j+1)*hw));
+		uvs.Add(new Vector2(0,1));
+		vertices.Add(new Vector3((i+1)*hw,level,(j+1)*hw));
+		uvs.Add(new Vector2(1,1));
+		
+		triangles.Add(idx);
+		triangles.Add(idx+2);
+		triangles.Add(idx+1);
+		
+		triangles.Add(idx+2);
+		triangles.Add(idx+3);
+		triangles.Add(idx+1);
+
+	}*/
+	void AttachVRect(Vector2 p1, Vector3 p2, float z1, float z2)
+	{
+		int idx = vertices.Count;
+		vertices.Add(new Vector3(p1.x,z1,p1.y));
+		uvs.Add(new Vector2(0,0));
+		vertices.Add(new Vector3(p2.x,z1,p2.y));
+		uvs.Add(new Vector2(0,1));
+		vertices.Add(new Vector3(p1.x,z2,p1.y));
+		uvs.Add(new Vector2(1,0));
+		vertices.Add(new Vector3(p2.x,z2,p2.y));
+		uvs.Add(new Vector2(1,1));
+
+		triangles.Add(idx);
+		triangles.Add(idx+1);
+		triangles.Add(idx+2);
+		
+		triangles.Add(idx+1);
+		triangles.Add(idx+3);
+		triangles.Add(idx+2);
+
+
+	}
+
 	void GenerateMesh(bool editMode)
 	{
 
 		Mesh mesh = new Mesh();
 
 
-		List<Vector3> vertices = new List<Vector3>();
-
-		List<int> triangles = new List<int>();
+		vertices.Clear();
+		triangles.Clear();
+		uvs.Clear();
 
 		int h = map.GetUpperBound(0);
 		int w = map.GetUpperBound(1);
@@ -82,116 +134,76 @@ public class TerrainController : MonoBehaviour {
 			for(int j=0;j<=w;j++)
 			{
 				Cell c = map[i,j];
-				c.lt=-1;
-				c.lb=-1;
-				c.rt=-1;
-				c.rb=-1;
-				float level = c.Digged?hw:0;
-				//Debug.Log("lt="+c.lt+" rt="+c.rt);
-				if(j>0 && c.Digged==map[i,j-1].Digged)
-				{
-					c.lt=map[i,j-1].rt;
-					c.lb=map[i,j-1].rb;
-				}
-
-				if(i>0 && c.Digged==map[i-1,j].Digged)
-				{
-					c.lt=map[i-1,j].lb;
-					c.rt=map[i-1,j].rb;
-				}
-
-				if(c.lt==-1)
-				{
-					c.lt=index;
-					vertices.Add(new Vector3(j*hw,level,i*hw));
-					index++;
-				}
-				if(c.rt==-1)
-				{
-					c.rt=index;
-					vertices.Add(new Vector3((j+1)*hw,level,i*hw));
-					index++;
-				}
-				if(c.lb==-1)
-				{
-					c.lb=index;
-					vertices.Add(new Vector3(j*hw,level,(i+1)*hw));
-					index++;
-				}
-				if(c.rb==-1)
-				{
-					c.rb=index;
-					vertices.Add(new Vector3((j+1)*hw,level,(i+1)*hw));
-					index++;
-				}
-
-				triangles.Add(c.lt);
-				triangles.Add(c.lb);
-				triangles.Add(c.rt);
-
-				triangles.Add(c.rt);
-				triangles.Add(c.lb);
-				triangles.Add(c.rb);
-
-				if(j>0 && c.Digged!=map[i,j-1].Digged)
-				{
-					triangles.Add(map[i,j-1].rt);
-					triangles.Add(map[i,j-1].rb);
-					triangles.Add(c.lb);
+				int idx = vertices.Count;
+				float level = c.Digged?0:hw;
 
 
-					triangles.Add(map[i,j-1].rt);
-					triangles.Add(c.lb);
-					triangles.Add(c.lt);
-				}
+				vertices.Add(new Vector3(i*hw,level,j*hw));
+				uvs.Add(new Vector2(0,0));
+				vertices.Add(new Vector3((i+1)*hw,level,j*hw));
+				uvs.Add(new Vector2(1,0));
+				vertices.Add(new Vector3(i*hw,level,(j+1)*hw));
+				uvs.Add(new Vector2(0,1));
+				vertices.Add(new Vector3((i+1)*hw,level,(j+1)*hw));
+				uvs.Add(new Vector2(1,1));
 
-				if(i>0 && c.Digged!=map[i-1,j].Digged)
+				triangles.Add(idx);
+				triangles.Add(idx+2);
+				triangles.Add(idx+1);
+
+				triangles.Add(idx+2);
+				triangles.Add(idx+3);
+				triangles.Add(idx+1);
+
+				if(c.Digged)
 				{
-					triangles.Add(map[i-1,j].rb);
-					triangles.Add(map[i-1,j].lb);
-					triangles.Add(c.lt);
+					if(j>0 && map[i,j-1].Digged==false)
+					{
+						AttachVRect(
+									new Vector2(	(i+1)	*hw, j		*hw),
+									new Vector2(	i		*hw, j		*hw),
+						            hw,0);
+					}
+
+					if(i>0 && map[i-1,j].Digged==false)
+					{
+						AttachVRect(new Vector2(	i		*hw, j		*hw),
+						            new Vector2(	i		*hw, (j+1)	*hw),
+						            hw,0);
+					}
+
+					if(j<w && map[i,j+1].Digged==false)
+					{
+						AttachVRect(
+							new Vector2(	i		*hw, (j+1)	*hw),
+							new Vector2(	(i+1)	*hw, (j+1)	*hw),
+							hw,0);
+					}
 					
-					
-					triangles.Add(map[i-1,j].rb);
-					triangles.Add(c.lt);
-					triangles.Add(c.rt);
+					if(i<h && map[i+1,j].Digged==false)
+					{
+						AttachVRect(
+									new Vector2(	(i+1)	*hw, (j+1)	*hw),
+									new Vector2(	(i+1)	*hw, j		*hw),
+						            hw,0);
+					}
 				}
 			}
 		}
-		//Debug.Log("index = "+index);
 
-
-		/*int h = map.GetUpperBound(0)+1;
-		int w = map.GetUpperBound(1)+1;
-		for(int i=0;i<h;i++)
-		{
-			for(int j=0;j<w;j++)
-			{
-
-				vertices.Add(new Vector3(j*hw,0,i*hw));
-				if(j>0 && i>0)
-				{
-					triangles.Add(i*w+j);
-					triangles.Add((i-1)*w+j);
-					triangles.Add(i*w+j-1);
-
-
-					triangles.Add((i-1)*w+j-1);
-					triangles.Add(i*w+j-1);
-					triangles.Add((i-1)*w+j);
-
-				}
-
-			}
-		}*/
 
 		mesh.vertices = vertices.ToArray();
 		mesh.triangles = triangles.ToArray();
+		mesh.uv = uvs.ToArray();
 		mesh.RecalculateNormals();
 
 		if(editMode)
 			GetComponent<MeshFilter>().sharedMesh=mesh;
 		else 
 			GetComponent<MeshFilter>().mesh = mesh;
+
+		vertices.Clear();
+		triangles.Clear();
+		uvs.Clear();
 	}
 }
