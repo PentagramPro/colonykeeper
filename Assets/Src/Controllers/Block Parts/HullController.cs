c9c787823a88269534229e4a4032b730b282ed0e
PentagramPro <maxon-line@yandex.ru>
2014-03-01 03:43:56 +0400

    Preparations for implementation of battle system

---
+++ Assets/Src/Controllers/Block Parts/HullController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HullController : MonoBehaviour {
+
+
+	int maxHP = 1000;
+	public int MaxHP{
+		get{
+			return maxHP;
+		}
+	}
+
+	int curHP = 0;
+	public int CurHP{
+		get{
+			return curHP;
+		}
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		curHP = maxHP;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
